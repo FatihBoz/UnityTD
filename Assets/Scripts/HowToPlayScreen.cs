9ae33abb56e46e53f878eb5174d875b6cf8d7314
Fatih <faatihboz@gmail.com>
2024-04-21 12:45:14 +0300

    game balancing

--- Assets/Scripts/HowToPlayScreen.cs
+++ Assets/Scripts/HowToPlayScreen.cs
@@ -9,24 +9,31 @@ using System;
 public class HowToPlayScreen : MonoBehaviour
 {
     [SerializeField] List<TextMeshProUGUI> texts;
-    [SerializeField] Image skullIMG;
+    [SerializeField] List<Image> images;
     [SerializeField] GameObject touchText;
 
     private int textOrder = 0;
+    private readonly float fadeTime = 1f;
 
     public void OnPanelClick()
     {
 
         if (textOrder < texts.Count)
         {
-            texts[textOrder].DOFade(1, 1f);
+            texts[textOrder].DOFade(1, fadeTime);
             textOrder++;
         }
         else if(textOrder == texts.Count)
         {
-            skullIMG.DOFade(1, 1f);
+            images[0].DOFade(1, fadeTime);
+            textOrder++;
+        }
+        else if (textOrder == texts.Count + 1)
+        {
+            images[1].DOFade(1, fadeTime);
             touchText.SetActive(false);
         }
+
 
     }
 }
